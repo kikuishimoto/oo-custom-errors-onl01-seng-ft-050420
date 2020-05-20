62f3013a6a4f20d45d0c15073ee6fa4746f58b7c
kiku ishimoto <kikuish@gmail.com>
2020-05-20 23:21:48 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,7 +9,7 @@ class Person
     self.partner = person
     if person.class != Person
       raise PartnerError
-    else 
+    else
       person.partner = self
     end
   end
@@ -21,9 +21,3 @@ end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
-
-
